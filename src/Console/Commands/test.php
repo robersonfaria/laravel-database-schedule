e0edc04346f74bd8819b81c31cd6b525535fd43b
Roberson Faria <roberson.faria@gmail.com>
2021-03-20 12:20:23 -0300

    command test

---
+++ src/Console/Commands/test.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to test operation.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Hello the test worked.');
+        \Log::info('Hello the test worked.');
+        return 0;
+    }
+}
