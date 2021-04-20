262748ccdda56b76155d74a3265c57cb53a77d3b
Pedro Paulo Silva <p.paulo0512@gmail.com>
2021-04-20 12:04:35 -0300

    refactor: saving the schedule in history before the finish of event

--- src/Console/Scheduling/Schedule.php
+++ src/Console/Scheduling/Schedule.php
@@ -32,15 +32,6 @@ class Schedule extends BaseSchedule
             $event->name(md5($command . json_encode($schedule->mapArguments() ?? [])))
                 ->cron($schedule->expression);
 
-            $event->after(function () use ($schedule, $event, $command) {
-                $schedule->histories()->create([
-                    'command' => $command,
-                    'params' => $schedule->params,
-                    'options' => $schedule->options,
-                    'output' => file_get_contents($event->output)
-                ]);
-            });
-
             if ($schedule->even_in_maintenance_mode) {
                 $event->evenInMaintenanceMode();
             }
@@ -73,6 +64,14 @@ class Schedule extends BaseSchedule
                 $event->onOneServer();
             }
 
+            $event->before(function () use ($schedule, $event, $command) {
+                $schedule->histories()->create([
+                    'command' => $command,
+                    'params' => $schedule->params,
+                    'options' => $schedule->options,
+                    'output' => file_get_contents($event->output)
+                ]);
+            });
             unset($event);
         }
 
